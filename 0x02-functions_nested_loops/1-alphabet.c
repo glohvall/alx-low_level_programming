f715b61af70d2cf9aa71d5b72f32c7f6e3c5aa8a
Glory <gloryvalery574@gmail.com>
2022-09-14 11:20:34 +0300

    alphabet c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print alphabet
+ *return: 0 if success
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+		_putchar('c');
+	_putchar('\n');
+}
