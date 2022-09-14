6cc2eba10651c4a3f6d4711fa02ea5e6531c3954
Glory <gloryvalery574@gmail.com>
2022-09-14 15:22:00 +0300

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isalpha - print both lower case and upper case alphabets else print zero
+ *@c: the character to print
+ *Return: return 1 if lower case, otherwise return 0
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 97, 67 && c <= 122, 90)
+		return (1);
+	else
+		return (0);
+}
