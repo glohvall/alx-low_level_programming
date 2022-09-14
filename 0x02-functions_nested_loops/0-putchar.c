2fdd5a487c47e4bf07e8fab3e3af5e3001db4056
Glory <gloryvalery574@gmail.com>
2022-09-14 10:08:54 +0300

    putchar2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *print _putchar
+ *main -program that prints _putchar
+ *Return: 0 if success
  */
 
 int main(void)
@@ -15,5 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
