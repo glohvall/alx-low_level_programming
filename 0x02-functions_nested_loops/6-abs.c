11d97b33f9fe315f8c5f2a3161a432546a70d48d
Glory <gloryvalery574@gmail.com>
2022-09-14 18:21:58 +0300

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - prints absolute value
+ *Return: absolute value
+ *@g: the integer
+*/
+
+int _abs(int g)
+{
+	if (g >= 0)
+		return (g);
+	else
+		return (-g);
+}
