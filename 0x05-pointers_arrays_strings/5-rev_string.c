eed61d793e18ffce24602d74977caf1b107d5396
Glory <gloryvalery574@gmail.com>
2022-09-20 00:37:32 +0300

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,16 @@
 
 void rev_string(char *s)
 {
-	
+	int i = 0;
+	int len;
+	char **rev_str = &s;
+
+	len = _strlen(s);
+
+	while (len >= i)
+	{
+		len--;
+		**(rev_str + i) = (*(s + len));
+		i++;
+	}
 }
