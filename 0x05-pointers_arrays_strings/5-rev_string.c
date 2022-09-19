5fddd392c508bf1243e021c2e695b96b59fd5a09
Glory <gloryvalery574@gmail.com>
2022-09-20 00:55:12 +0300

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,16 @@
 
 void rev_string(char *s)
 {
-	int i = 0, a = 0, Aux;
+	int len = 0, index = 0;
+	char tmp;
 
+	while (s[index++])
+		len++;
 
-	while (s[i] != '\0')
+	for (index = len - 1; index >= len / 2; index--)
 	{
-		i++;
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
 	}
-	i--;
-	while (i > a)
-	{
-		Aux = s[i];
-		s[i--] = s[a];
-	}		s[a++] = Aux;
 }
