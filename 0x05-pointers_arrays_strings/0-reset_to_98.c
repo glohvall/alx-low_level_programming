865b34a868dafcc95a8d2b3d15485c119de6d18f
Glory <gloryvalery574@gmail.com>
2022-09-19 15:55:41 +0300

    first pointer try

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - pointer that changes value to 98
+ *@n: pointer to address whose value is 98
+ *Return: 0 if success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
