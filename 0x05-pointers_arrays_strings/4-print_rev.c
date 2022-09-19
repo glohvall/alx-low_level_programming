a6d359cbe431e54c6b72c8308ee5403fcaad6037
Glory <gloryvalery574@gmail.com>
2022-09-20 00:28:54 +0300

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,6 +8,19 @@
 
 void print_rev(char *s)
 {
-	while (*s)
-		_putchar(*s--);
+	int i;
+	int j;
+	int len;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar (s[j]);
+	}
+	_putchar ('\n');
 }
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *rev_string - function that reverses a string
+ *@s: pointer
+ *Return: o
+ */
+
+void rev_string(char *s)
+{
+	
+}
