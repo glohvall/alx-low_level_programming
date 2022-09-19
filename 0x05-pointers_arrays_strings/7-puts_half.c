c8a0fc291c920d2dd6de2c642b2ed358730cd853
Glory <gloryvalery574@gmail.com>
2022-09-20 01:36:41 +0300

    main

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * puts_half - Prints half of a string.
+ * @str: The string to be printed.
  */
-
-int main(void)
+void puts_half(char *str)
 {
-	char *str;
+	int index = 0, len = 0, n;
 
-	str = "0123456789";
-	puts_half(str);
-	return (0);
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+	else
+		n = (len + 1) / 2;
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
 }
