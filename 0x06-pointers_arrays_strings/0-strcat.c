073af3f6935f9f9f9c59228fcaf7fdb376d0c420
Glory <gloryvalery574@gmail.com>
2022-09-22 09:29:59 +0300

    took a whole day

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcat - appends the strig in *src into *dest
+ *@dest: destination variable
+ *@src: source variable
+ *Return: destination variable
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len_dest = 0;
+	int len_src = 0;
+
+	while (dest[len_dest])
+	{
+		len_dest++;
+	}
+	for (; src[len_src] != '\0' ;)
+	{
+		dest[len_dest++] = src[len_src++];
+	}
+	dest[len_dest] = '\0';
+
+	return(dest);
+}
