dcc16225c9c32aebe4d4add4af2f7a1afbdf251a
Glory <gloryvalery574@gmail.com>
2022-09-22 17:16:24 +0300

    replace lower case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ **string_toupper - print capital alphabet
+ *Return: 0 if success
+ */
+
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		i++;
+	}
+	return (str);
+}
