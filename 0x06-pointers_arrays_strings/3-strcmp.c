68cca27979fb561a740dc09abfeabbe8743f54e7
Glory <gloryvalery574@gmail.com>
2022-09-22 14:05:23 +0300

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,9 +14,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (; (s1[i] == s2[i]); i++)
 		return (0);
-
-	/*for (; (s1[i] != s2[i]); i++)*/
-		result = (s1[i] - s2[i]);
+	result = (s1[i] - s2[i]);
 
 	return (result);
 }
