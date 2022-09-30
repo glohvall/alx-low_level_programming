5be3d7f928b5676f15d5fccdc04b072620d4fc73
Glory <gloryvalery574@gmail.com>
2022-09-30 12:08:24 +0300

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print name of file
+ *@argc: argument count
+ *@argv: number of arguments compiled
+ *Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
