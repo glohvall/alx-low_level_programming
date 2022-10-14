6dba6a1b92041e56d01303941c390dab27ba7c2c
Glory <gloryvalery574@gmail.com>
2022-10-14 15:46:02 +0300

    trial 63

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,12 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
  * sum_them_all - returns the sum of all parameters
  * @n: the numbers of arguments being passed through
  * Return: the sum of all parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
